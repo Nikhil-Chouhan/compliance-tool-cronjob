72a8e7913c1d646aaf0c4c0b08a0c68ce44ea220
Nikhil Chouhan <60143663+Nikhil-Chouhan@users.noreply.github.com>
2024-04-16 16:23:13 +0530

    changes

--- cron_server.js
+++ cron_server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const cron = require("node-cron");
 
 const app = express();
 
-const url = process.env.APP_URL || "http://localhost:3000";
-const port = process.env.PORT || 3000;
+const url = process.env.APP_URL;
+const port = process.env.CRON_PORT;
 
 const makeRequest = async () => {
   const response = await fetch(`${url}/api/trigger-cronjobs`);
@@ -15,7 +16,7 @@ const makeRequest = async () => {
 // 0 8 * * *  -- everyday 8 AM 
 // */1 * * * * -- after every minute
 
-cron.schedule("0 8 * * *", function () {
+cron.schedule("*/1 * * * *", function () {
   makeRequest();
   console.log("Running cron job at 8 in the morning");
 });
